17e7915b7f0a049b465b5c6e88f01bafe9a66e25
Christian Sundin <christiansundin@gmail.com>
2018-04-25 19:58:19 +0200

    Solved lecture 6 task1 completely. Started on task2

--- lecture_6_task1.sh
+++ lecture_6_task1.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 echo "this is the name of the file: $0"
-if [ $# ==  0 ]; then
+if [ "$#" ==  0 ]; then
  exit
 fi
-if [ $1 != 0 ]; then
+if [ "$1" != 0 ]; then
  echo "argument 1: ${1}"
 fi
-if [ $2 !=  3 ]; then
-echo "argument 4: ${3}"
+if [ "$2" != 0 ]; then
+echo "argument 3: ${3}"
 fi
-if [ ${10} != 10 ]; then
+if [ "${10}" != "" ]; then
 echo "argument 10: ${10}"
 fi
 echo "The number of arguments: $#"
+if [ $# > 3 ]; then
+shift 3
+fi
+echo "$@"
+echo "The remaining nr of arguments: $#"
---
+++ lecture_6_task2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+for i in $@; do
+if [ "$i" == "--help" ]; then
+echo -e "\n help \n"
+fi
+done
+echo "Empty"
+echo "First we display the home directory:"
+echo $PWD 
+echo "Then the number of processes running by the user:"
+ps -c | grep -v 'PID' | wc -l
+echo "And the number of processes in total on the system: "
+ps -A | grep -v 'PID' | wc -l
+echo "Physical amount of memory in kilobytes: "
+awk '/MemTotal/{print $2}' /proc/meminfo
+echo "Swap space in kilobytes: "
+awk '/SwapTotal/{print $2}' /proc/meminfo
+
